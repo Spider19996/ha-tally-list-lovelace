b280d35ad58a4be0530c31f5c0688ecb9b8fd812
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-07-26 14:14:08 +0200

    Increase remove button padding

--- tally-list-card.js
+++ tally-list-card.js
@@ -374,6 +374,7 @@ class TallyListCard extends LitElement {
     .remove-bottom {
       text-align: left;
       margin-top: 8px;
+      padding-left: 20px;
     }
     .add-button {
       height: 32px;
