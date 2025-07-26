cc55e64b7d61a8c0e6cfbca3e5c8f7a69d88e7e7
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-07-26 14:09:08 +0200

    Revert "Fix remove button alignment"

--- tally-list-card.js
+++ tally-list-card.js
@@ -374,7 +374,6 @@ class TallyListCard extends LitElement {
     .remove-bottom {
       text-align: left;
       margin-top: 8px;
-      padding-left: 8px;
     }
     .add-button {
       height: 32px;
