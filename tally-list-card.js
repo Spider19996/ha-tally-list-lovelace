ed0e4b4070e06fbeb9f48b90504abf5e541a9675
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-07-26 14:17:43 +0200

    Update tally-list-card.js

--- tally-list-card.js
+++ tally-list-card.js
@@ -374,7 +374,7 @@ class TallyListCard extends LitElement {
     .remove-bottom {
       text-align: left;
       margin-top: 8px;
-      padding-left: 22px;
+      padding-left: 21px;
     }
     .add-button {
       height: 32px;
