f3478607540be6e10f445d922f92a9f2380f0fe7
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-08-08 08:48:12 +0200

    Bump Version Number

--- tally-list-card.js
+++ tally-list-card.js
@@ -1,6 +1,6 @@
 // Tally List Card
 import { LitElement, html, css } from 'https://unpkg.com/lit?module';
-const CARD_VERSION = 'v06.08.2025';
+const CARD_VERSION = '08.08.2025';
 
 const TL_STRINGS = {
   en: {
