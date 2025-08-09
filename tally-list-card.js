67678fd19fba4e878796f9cafcccecf2a02e4c31
Robin Zimmermann <53911689+Spider19996@users.noreply.github.com>
2025-08-09 11:29:49 +0200

    Update tally-list-card.js

--- tally-list-card.js
+++ tally-list-card.js
@@ -1,7 +1,7 @@
 // Tally List Card
 import { LitElement, html, css } from 'https://unpkg.com/lit?module';
 import { repeat } from 'https://unpkg.com/lit/directives/repeat.js?module';
-const CARD_VERSION = '08.08.2025';
+const CARD_VERSION = '09.08.2025';
 
 const TL_STRINGS = {
   en: {
